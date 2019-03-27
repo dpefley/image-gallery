a2fe7016bf28ea281bdf82ce27263ac5910f02a0
dpefley <dpefley@wustl.edu>
2019-03-27 12:52:00 -0500

    werkin it

---
+++ main.js
@@ -0,0 +1,41 @@
+document.getElementById("nature-1").addEventListener("click", function() {
+  loadLarge("nature-1");
+});
+document.getElementById("nature-2").addEventListener("click", function() {
+  loadLarge("nature-2");
+});
+document.getElementById("nature-3").addEventListener("click", function() {
+  loadLarge("nature-3");
+});
+document.getElementById("nature-4").addEventListener("click", function() {
+  loadLarge("nature-4");
+});
+document.getElementById("nature-5").addEventListener("click", function() {
+  loadLarge("nature-5");
+});
+document.getElementById("nature-6").addEventListener("click", function() {
+  loadLarge("nature-6");
+});
+document.getElementById("nature-7").addEventListener("click", function() {
+  loadLarge("nature-7");
+});
+document.getElementById("nature-8").addEventListener("click", function() {
+  loadLarge("nature-8");
+});
+document.getElementById("nature-9").addEventListener("click", function() {
+  loadLarge("nature-9");
+});
+document.getElementById("nature-10").addEventListener("click", function() {
+  loadLarge("nature-10");
+});
+document.getElementById("nature-11").addEventListener("click", function() {
+  loadLarge("nature-11");
+});
+document.getElementById("nature-12").addEventListener("click", function() {
+  loadLarge("nature-12");
+});
+
+function loadLarge(nature) {
+  console.log("clicked " + nature);
+
+}
